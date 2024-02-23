80998cb624c34e06682f0428f56c619b8ce93cf4
xu4wei <149287575+xu4wei@users.noreply.github.com>
2024-02-23 22:08:18 +0800

    Statistics read file contents
    Cooperate with word_count.tst
    Count lines, words, chars

---
+++ 100DaysOfCode_009.py
@@ -0,0 +1,13 @@
+line_count = 0
+word_count = 0
+char_count = 0
+
+with  open('word_count.tst') as infile:
+    for line in infile:
+        line_count += 1
+        char_count += len(line)
+        words = line.split()
+        word_count += len(words)
+
+print("File has {0} lines, {1} words, {2} characters".format(line_count, 
+      word_count, char_count))
