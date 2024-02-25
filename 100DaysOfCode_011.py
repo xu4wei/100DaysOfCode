7d9c76f16942db281b97183c7257ac470dbcd90b
xu4wei <149287575+xu4wei@users.noreply.github.com>
2024-02-25 21:06:00 +0800

    If/else
    If/else statement prints which matching choice

---
+++ 100DaysOfCode_011.py
@@ -0,0 +1,12 @@
+small = True
+green = False
+if small:
+    if green:
+        print("cherry")
+    else:
+        print("pea")
+else:
+    if green:
+        print("watermelon")
+    else:
+        print("pumpkin")
