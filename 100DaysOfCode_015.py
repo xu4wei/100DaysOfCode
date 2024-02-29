2d26b64d6ae2d6af802b7e37bf29215a0e4e2984
xu4wei <149287575+xu4wei@users.noreply.github.com>
2024-02-29 22:53:52 +0800

    string
    upper()
    lower()
    replace(' a',' A')

---
+++ 100DaysOfCode_015.py
@@ -0,0 +1,11 @@
+song = """Wine comes in at the mouth
+And love comes in at the eye;
+That’s all we know for truth
+Before we grow and die.
+I lift the glass to my mouth,
+I look at you, and I sigh."""
+
+print(song.upper(),end="\n\n")
+print(song.lower(),end="\n\n")
+song = song.replace(" a"," A")
+print(song)
