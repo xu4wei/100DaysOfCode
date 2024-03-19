981d17880613ce029cf8b41012a7bce3e68dabe2
xu4wei <149287575+xu4wei@users.noreply.github.com>
2024-03-19 12:09:58 +0800

    Update 100DaysOfCode_033.py
    Indentation adjustment

--- 100DaysOfCode_033.py
+++ 100DaysOfCode_033.py
@@ -1,24 +1,24 @@
 class Solution:
-   def reverseVowels(self, s):
-      chars = list(s)
-      vowel = ['a','e','i','o','u']
-      index = []
-      vowels = []
-      for i in range(len(chars)):
-         if chars[i] in vowel:
-            vowels.append(chars[i])
-            index.append(i)
-      vowels = vowels[::-1]
-      final = []
-      ind = 0
-      for i in range(len(chars)):
-         if i in index:
-            final.append(vowels[ind])
-            ind += 1
-         else:
-            final.append(chars[i])
-      str1 = ""
-      return str1.join(final)
+	 def reverseVowels(self, s):
+        chars = list(s)
+        vowel = ['a','e','i','o','u']
+        index = []
+        vowels = []
+        for i in range(len(chars)):
+            if chars[i] in vowel:
+                vowels.append(chars[i])
+                index.append(i)
+        vowels = vowels[::-1]
+        final = []
+        ind = 0
+        for i in range(len(chars)):
+            if i in index:
+                final.append(vowels[ind])
+                ind += 1
+            else:
+                final.append(chars[i])
+        str1 = ""
+        return str1.join(final)
 
 
 s = Solution()
